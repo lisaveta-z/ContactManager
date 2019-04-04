13fed193432227f70a31db770333fe64fc89b127
lisaveta-z <lisaveta.z@rambler.ru>
2019-04-04 15:35:20 +0300

    add Contact model

---
+++ ContactManager/Models/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using ContactManager.Models.Validation;
+
+namespace ContactManager.Models
+{
+    public class Contact
+    {
+        [Required(ErrorMessage = "Last name is required")]
+        [RegularExpression(@"^(\S|\D)*$", ErrorMessage = "No white spaces or numbers allowed")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [RegularExpression(@"^(\S|\D)*$", ErrorMessage = "No white spaces or numbers allowed")]
+        public string Name { get; set; }
+
+        [RegularExpression(@"^(\S|\D)*$", ErrorMessage = "No white spaces or numbers allowed")]
+        public string MiddleName { get; set; }
+
+        [Required(ErrorMessage = "Birth date is required")]
+        [CurrentDate(ErrorMessage = "Date must not be greater than current")]
+        public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "Tag is required")]
+        [StringLength(64, MinimumLength = 2, ErrorMessage = "Tag length must be between 2 and 64 characters")]
+        public string Tag { get; set; }
+    }
+}
---
+++ ContactManager/Models/Validation/CurrentDateAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactManager.Models.Validation
+{
+    public class CurrentDateAttribute : ValidationAttribute
+    {
+        public CurrentDateAttribute() : base() 
+        {
+        }
+
+        public override bool IsValid(object value)
+        {
+            return (DateTime)value <= DateTime.Now;
+        }
+    }
+}
